c8489a3f9dd676bec5b234580af955b2e1af85c4
selingindev <88356338+selingindev@users.noreply.github.com>
2025-12-30 14:55:39 -0300

    separnado contorlçler d emodel

--- truco/src/baralho/BaralhoModel.java
+++ truco/src/baralho/BaralhoModel.java
@@ -4,54 +4,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 import cartas.CartaModel;
-import cartas.EnumNipe;
-import cartas.EnumNumero;
-import jogador.JogadorModel;
 
 public class BaralhoModel {
     private List<CartaModel> cartas = new ArrayList<>();
     private CartaModel vira;
-    
+
+
     public BaralhoModel() {
-        buildCartas();
     }
 
     public List<CartaModel> getCartas() {
         return cartas;
     }
 
-    private void buildCartas() {
-        int peso = 4;
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 4; j++) {
-                cartas.add(new CartaModel(EnumNumero.values()[i], peso, EnumNipe.values()[j]));
-            }
-            peso++;
-        }
-        for (CartaModel carta : cartas) {
-            System.out
-                    .println("Carta: " + carta.getNumero() + " de " + carta.getNipe());
-        }
+    public void setCartas(List<CartaModel> cartas) {
+        this.cartas = cartas;
     }
 
-    private void removerCarta(CartaModel carta) {
-        cartas.remove(carta);
+    public CartaModel getVira() {
+        return vira;
     }
 
-    private CartaModel virarCarta() {
-        int idx = (int) (Math.random() * cartas.size());
-        CartaModel carta = cartas.get(idx);
-        removerCarta(cartas.get(idx));
-        return carta;
+    public void setVira(CartaModel vira) {
+        this.vira = vira;
     }
-
-    private void darCarta(List<JogadorModel> Jogadores) {
-        
-        for (JogadorModel jogador : Jogadores){
-             if(Jogadores.size() ) 
-        
-        } 
-
-    }
+   
 
 }
