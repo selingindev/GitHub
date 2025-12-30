761c96b2071707bf950387094306aba7b057a3ef
selingindev <88356338+selingindev@users.noreply.github.com>
2025-12-30 14:55:58 -0300

    comnelçando usar o app para teste

--- truco/src/App.java
+++ truco/src/App.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import baralho.BaralhoController;
+import baralho.BaralhoModel;
+import jogador.JogadorModel;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        BaralhoModel baralhoModel = new BaralhoModel();
+        BaralhoController baralhoController = new BaralhoController();
+        List<JogadorModel> jogadores = new ArrayList<>();
+
+        JogadorModel jogador1 = new JogadorModel("Player1", null);
+        JogadorModel jogador2 = new JogadorModel("Player2", null);
+
+        jogadores.add(jogador1);
+        jogadores.add(jogador2);
+    
+        baralhoController.buildCartas();
+        baralhoController.destribuirCartas()jogadores;
+
     }
 }
